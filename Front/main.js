caeeddaa530be31861fa2003dab2a6d2ef4c6646
Vinicius Bittencourt <vinisataides@gmail.com>
2020-10-26 13:17:42 -0300

    Fetch Register + localStorage

--- Front/main.js
+++ Front/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var switchdrop = false;
 
+    // Button to open submenu on dashboard manu
     $("#drop-sub").click(function () {
         if (switchdrop == false) {
             $(".sub").css("display", "block");
@@ -11,6 +12,7 @@ $(document).ready(function () {
         }
     });
 
+    //Button to Authenticate user
     $("#signinbtn").click(function (e) {
         e.preventDefault();
 
@@ -25,26 +27,67 @@ $(document).ready(function () {
         postData("http://localhost:3333/auth/authenticate", dataUser).then(
             (response) => {
                 console.log(response);
+
+                // if (!response.ok) {
+                //     console.log("Deu ruim");
+                //     throw new Error("Network response was not ok");
+                // }
                 localStorage.setItem("userToken", response.token);
+                var url = "http://127.0.0.1:5500/dashboard.html";
+                window.location = url;
             }
         );
     });
 
-    async function postData(url = "", data = {}) {
-        // Default options are marked with *
-        const response = await fetch(url, {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: "follow", // manual, *follow, error
-            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        });
-        return response.json(); // parses JSON response into native JavaScript objects
-    }
+    //Button to exit dashboard and clean localStorage
+    $("#btnsair").click(function (e) {
+        e.preventDefault();
+        localStorage.clear();
+        var url = "http://127.0.0.1:5500/index.html";
+        window.location = url;
+    });
+
+    //Button to register a user
+    $("#btn-register").click(function (e) {
+        e.preventDefault();
+        console.log("Registro");
+        var nameRegister = $("#registerName").val();
+        var emailRegister = $("#registerEmail").val();
+        var passwordRegister = $("#registerPassword").val();
+
+        const dataRegister = {
+            email: emailRegister,
+            password: passwordRegister,
+            name: nameRegister,
+        };
+
+        postData("http://localhost:3333/auth/register", dataRegister).then(
+            (response) => {
+                console.log(response);
+
+                localStorage.setItem("userToken", response.token);
+                var url = "http://127.0.0.1:5500/dashboard.html";
+                window.location = url;
+            }
+        );
+    });
 });
+
+//Function to postData
+async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
