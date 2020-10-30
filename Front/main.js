d022ce5a7b00df0e90b7131738f4168dc0ecdc66
Vinicius Bittencourt <vinisataides@gmail.com>
2020-10-30 13:24:46 -0300

    Flask API

--- Front/main.js
+++ Front/main.js
@@ -12,6 +12,25 @@ $(document).ready(function () {
         }
     });
 
+    //Button that makes a GET request to the Flask API
+    $("#btn-procedure").click(async function (e) {
+        e.preventDefault();
+        console.log("cli");
+        const respon = await fetch("http://127.0.0.1:5000/", {
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url,
+        });
+        console.log(respon);
+    });
+
     //Button to Authenticate user
     $("#signinbtn").click(function (e) {
         e.preventDefault();
