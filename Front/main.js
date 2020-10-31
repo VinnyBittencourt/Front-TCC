2136a0d83fddce395c31cd4549c7f7461b3d8e27
Vinicius Bittencourt <vinisataides@gmail.com>
2020-10-30 22:06:42 -0300

    Alterações

--- Front/main.js
+++ Front/main.js
@@ -28,7 +28,8 @@ $(document).ready(function () {
             redirect: "follow", // manual, *follow, error
             referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url,
         });
-        console.log(respon);
+        const jres = respon.json();
+        console.log(jres);
     });
 
     //Button to Authenticate user
